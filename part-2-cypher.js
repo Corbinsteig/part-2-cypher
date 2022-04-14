4dfc860e0dbb0ed1d12e610ad63ce8b6107843b6
Corbin Steig <corsteig1@gmail.com>
2022-04-14 15:52:40 -0700

    first commit

---
+++ part-2-cypher.js
@@ -0,0 +1,14 @@
+let str = "I Love To Code!";
+for(let i = 0; i < str.length; i++) {
+    let asciiValue = str.charCodeAt(i);
+    let stringValue = String.fromCharCode(asciiValue);
+{
+ console.log(asciiValue) 
+ console.log(stringValue) 
+}
+}
+
+// const nums = (73,32,76,111,118,101,32,84,111,32,67,111,100,101,33)
+    
+
+
